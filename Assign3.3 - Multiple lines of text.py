ab84142d121f39ffb39cb5d4ccfd90497497002d
rsevial <rebekahsevial@gmail.com>
2023-04-25 11:17:56 +0800

    Create Pseudocode

---
+++ Assign3.3 - Multiple lines of text.py
@@ -0,0 +1,15 @@
+# Programmed by: Rebekah Joy E. Sevial
+# Problem 3 of Assignment 3
+# Multiple lines of text
+
+# Opening mylife.txt and assign as the main file
+# While loop asking the user for the lines
+# Ask the user to input lines
+# Input the lines in the text file
+# Inputted the lines in different line
+# Asking the user if are there more lines
+# If the user inputted "y", continue
+# Elif the user inputted "n", break
+# Else, invalid
+# Ask the user again if there are more lines
+# Break, if the user inputted "n"
