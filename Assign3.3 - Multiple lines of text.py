9a478ab75a7b99872d83fc70a9f036930ea3f9f7
rsevial <rebekahsevial@gmail.com>
2023-04-25 11:39:32 +0800

    Inputted the lines in different line

--- Assign3.3 - Multiple lines of text.py
+++ Assign3.3 - Multiple lines of text.py
@@ -9,7 +9,9 @@ with open("mylife.txt", "w") as main_file:
 # Ask the user to input lines
         inputted_lines = input("Enter line:")
 # Input the lines in the text file
+        main_file.write(inputted_lines)
 # Inputted the lines in different line
+        main_file.write("\n")
 # Asking the user if are there more lines
 # If the user inputted "y", continue
 # Elif the user inputted "n", break
